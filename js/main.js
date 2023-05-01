1e1a8edae6c04b8b95c58495279be4f830d61dc7
Patrick <patrykromanski11@gmail.com>
2023-05-01 16:49:15 +0200

    Adding js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const bodyEl = document.body;
+const barEl = document.querySelector('.bar');
+const updateBar = () => {
+    let scrollPos = (window.scrollY / 
+    (bodyEl.scrollHeight - window.innerHeight)) * 100;
+    barEl.style.width = `${scrollPos}%`;
+    requestAnimationFrame(updateBar);
+};
+
+updateBar();
